f19311ca2904a3e628d04af41a73f10cc2134aaf
Girija Viswanathan <58586934+GirijaViswa@users.noreply.github.com>
2020-01-11 16:18:31 -0800

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Articles.new(title:"title",description:"desc")
+Articles.new(title:"title1",description:"desc1")
